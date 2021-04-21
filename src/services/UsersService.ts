9c0154d721db7eb52f57d4eb8db89cb6527fc770
Pedro Façanha <pedrofacpeix@gmail.com>
2021-04-21 20:24:53 -0300

    Aula 03

---
+++ src/services/UsersService.ts
@@ -0,0 +1,37 @@
+import { getCustomRepository, Repository } from 'typeorm';
+import { User } from '../entities/User';
+import { UsersRepository } from '../repositories/UsersRepository';
+
+class UsersService {
+  private usersRepository: Repository<User>;
+
+  constructor() {
+    this.usersRepository = getCustomRepository(UsersRepository);
+  }
+
+  async create(email: string) {
+    //verificar se o usuario existe
+
+    const userExists = await this.usersRepository.findOne({
+      email,
+    });
+
+    if (userExists) {
+      return userExists;
+    }
+
+    //se nao existir, salvar no DB
+
+    const user = this.usersRepository.create({
+      email,
+    });
+
+    await this.usersRepository.save(user);
+
+    //se existir retornar user
+
+    return user;
+  }
+}
+
+export { UsersService };
